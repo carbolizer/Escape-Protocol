607928e32eda9d18cad4e705c868875a810120dd
carbolizer <carbolizer3@gmail.com>
2026-05-05 14:45:38 -0500

    i've got a camera enemy type, a security badge that you can pickup that lets you leave the level, ive got a title screen, win screen, and a transition between all of the levels im adding a melee weapon and guards to fight

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     public float speed;
     public bool hasBadge = false;
+    public bool isHidden = false;
+    public bool canHide = false;
+
+    [Header("Combat")]
+    public GameObject meleeHitbox;
+    public float attackDuration = 0.2f;
+    private float attackTimer = 0f;
 
     private void Awake()
     {
@@ -46,11 +53,46 @@ public class PlayerController : MonoBehaviour
         {
             moveInput = Vector2.right;
         }
+
+        // HIDING LOGIC
+        // If the player is in a hiding spot AND holding the 'F' key
+        if (canHide && UnityEngine.InputSystem.Keyboard.current.fKey.isPressed)
+        {
+            isHidden = true;
+            // Make the player semi-transparent to prove they are hiding
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+        }
+        else
+        {
+            isHidden = false;
+            // Return to normal color
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        }
+
+        // COMBAT LOGIC
+        // If we press Spacebar and aren't already attacking
+        if (UnityEngine.InputSystem.Keyboard.current.spaceKey.wasPressedThisFrame && attackTimer <= 0)
+        {
+            meleeHitbox.SetActive(true); // Turn on the damage zone
+            attackTimer = attackDuration; // Start the cooldown
+        }
+
+        // Turn the hitbox back off after a split second
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0)
+            {
+                meleeHitbox.SetActive(false);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = moveInput * speed * Time.deltaTime;
     }
+
+
 
 }
