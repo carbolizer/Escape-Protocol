1ac3c825f36e9b863ed9977e8529b0fb7848c9e7
carbolizer <carbolizer3@gmail.com>
2026-05-12 18:58:41 -0500

    Revert "Pause Menu with Controls"
    This reverts commit b543e15520cb05bf954b7d65244e4a95633004f8.

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -5,23 +5,13 @@ public class HealthUI : MonoBehaviour
 {
     public PlayerHealth playerHealth;
     public Image[] hearts;
+
     public Sprite fullHeart;
     public Sprite emptyHeart;
 
-    void Start()
-    {
-        // Automatically find the player's health script in the scene if the slot is empty!
-        if (playerHealth == null)
-        {
-            playerHealth = FindAnyObjectByType<PlayerHealth>();
-        }
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        // Safety check: Do not run the loop if the player was destroyed or hasn't loaded yet
-        if (playerHealth == null) return;
-
         for (int i = 0; i < hearts.Length; i++)
         {
             if (i < playerHealth.currentHealth)
