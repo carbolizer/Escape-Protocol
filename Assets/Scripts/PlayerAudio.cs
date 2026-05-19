34e7cf6d8322c23ff7ca8bec7f7952b075dbe819
Leinad42 <153773009+Leinad42@users.noreply.github.com>
2026-05-18 22:13:24 -0500

    Added background music, footsteps for player, and attack sound

---
+++ Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip footstepClip;
+    public AudioClip attackClip;
+
+    public float footstepDelay = 0.45f;
+    private float footstepTimer;
+
+    void Update()
+    {
+        bool isMoving =
+            Input.GetAxisRaw("Horizontal") != 0 ||
+            Input.GetAxisRaw("Vertical") != 0;
+
+        if (isMoving)
+        {
+            footstepTimer -= Time.deltaTime;
+
+            if (footstepTimer <= 0f && !audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(footstepClip);
+                footstepTimer = footstepDelay;
+            }
+        }
+        else
+        {
+            footstepTimer = 0f;
+            audioSource.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.PlayOneShot(attackClip);
+        }
+    }
+}
